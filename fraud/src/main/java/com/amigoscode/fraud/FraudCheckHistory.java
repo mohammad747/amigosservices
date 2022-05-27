2161fe52e8ae557bc1633f5dde3023096c2138fd
mohammad hashemi <mohammad.hashemi747@gmail.com>
2022-05-27 23:36:13 +0430

    Microservice communication via HTTP - 2- Fraud Microservice

---
+++ fraud/src/main/java/com/amigoscode/fraud/FraudCheckHistory.java
@@ -0,0 +1,26 @@
+package com.amigoscode.fraud;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+public class FraudCheckHistory {
+    @Id
+    @SequenceGenerator(
+            name = "fraud_id_sequence",
+            sequenceName = "fraud_id_sequence"
+    )
+    @GeneratedValue(
+            strategy = GenerationType.SEQUENCE,
+            generator = "fraud_id_sequence"
+    )
+    private Integer id;
+}
