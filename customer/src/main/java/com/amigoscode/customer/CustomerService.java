35ec3f570aff78a27cd36bd0f18e0e15c852ac03
mohammad hashemi <mohammad.hashemi747@gmail.com>
2022-06-04 16:16:43 +0430

    Use Open Feign instead of restTemplate
    #1

--- customer/src/main/java/com/amigoscode/customer/CustomerService.java
+++ customer/src/main/java/com/amigoscode/customer/CustomerService.java
@@ -1,12 +1,15 @@
 package com.amigoscode.customer;
 
+import com.amigoscode.clients.fraud.FraudCheckResponse;
+import com.amigoscode.clients.fraud.FraudClient;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
 @Service
 public record CustomerService(
         CustomerRepository customerRepository,
-        RestTemplate restTemplate
+        RestTemplate restTemplate,
+        FraudClient fraudClient
 ) {
     public void registerCustomer(CustomerRegistrationRequest request) {
         Customer customer = Customer.builder()
@@ -18,12 +21,17 @@ public record CustomerService(
         // todo: check if email is not taken
         customerRepository.saveAndFlush(customer);
         // todo: check if fraudster
-        FraudCheckResponse fraudCheckResponse = restTemplate.getForObject(
-//   before eureka server   and the after      "http://localhost:8081/api/v1/fraud-check/{customerId}",
-                "http://FRAUD/api/v1/fraud-check/{customerId}",
-                FraudCheckResponse.class,
-                customer.getId()
-        );
+
+//        FraudCheckResponse fraudCheckResponse = restTemplate.getForObject(
+////   before eureka server   and the after      "http://localhost:8081/api/v1/fraud-check/{customerId}",
+//                "http://FRAUD/api/v1/fraud-check/{customerId}",
+//                FraudCheckResponse.class,
+//                customer.getId()
+//        );
+
+        FraudCheckResponse fraudCheckResponse =
+                fraudClient.isFraudster(customer.getId());
+
         if (fraudCheckResponse.isFraudster()) {
             throw new IllegalStateException("Fraudster");
         }
--- customer/src/main/java/com/amigoscode/customer/FraudCheckResponse.java
+++
@@ -1,4 +0,0 @@
-package com.amigoscode.customer;
-
-public record FraudCheckResponse(Boolean isFraudster) {
-}
